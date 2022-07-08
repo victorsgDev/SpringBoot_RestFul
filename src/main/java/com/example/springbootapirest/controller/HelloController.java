7becdf6753db81c0b1b8573b9891efd31d27e6ce
Victor Sanchez Grande <victorsg.dev@gmail.com>
2022-07-08 20:36:22 +0200

    add spring boot devTools

--- src/main/java/com/example/springbootapirest/controller/HelloController.java
+++ src/main/java/com/example/springbootapirest/controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.springbootapirest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String helloWorld(){
+    @GetMapping("/")
+    public String helloWorld() {
         return "Bienvenido a mi SpringBootApiRestApplication";
     }
 }
