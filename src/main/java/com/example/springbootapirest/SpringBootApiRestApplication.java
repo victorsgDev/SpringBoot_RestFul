ac24f4c0b5fedc494f441a41dd2430a2ad6467d7
Victor Sanchez Grande <victorsg.dev@gmail.com>
2022-07-08 20:05:21 +0200

    First commit

---
+++ src/main/java/com/example/springbootapirest/SpringBootApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiRestApplication.class, args);
+    }
+
+}
