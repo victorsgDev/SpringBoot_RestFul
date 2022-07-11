ffdad4672bd71d254fb963d6db9685fdee8932fe
Victor Sanchez Grande <victorsg.dev@gmail.com>
2022-07-11 21:08:06 +0200

    Adding Logger info

--- src/main/java/com/example/springbootapirest/controller/DepartmentController.java
+++ src/main/java/com/example/springbootapirest/controller/DepartmentController.java
@@ -2,37 +2,44 @@ package com.example.springbootapirest.controller;
 
 import com.example.springbootapirest.entity.Department;
 import com.example.springbootapirest.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
 public class DepartmentController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
 
     @Autowired
     private DepartmentService departmentService;
 
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("saveDepartment from Controller");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("fetchDepartmentList from Controller");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
     public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        LOGGER.info("fetchDepartmentById from Controller");
         return departmentService.fetchDepartmentById(departmentId);
     }
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        LOGGER.info("deleteDepartmentById from Controller");
         departmentService.deleteDepartmentById(departmentId);
         return "¡Departamento eliminado con exito!";
     }
