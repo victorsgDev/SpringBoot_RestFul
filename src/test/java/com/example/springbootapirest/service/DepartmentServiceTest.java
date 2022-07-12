bc44eb9312131aef4d92a7ccdccae7a53a72a1f9
Victor Sanchez Grande <victorsg.dev@gmail.com>
2022-07-12 17:30:58 +0200

    Unit Testing Service Layer

---
+++ src/test/java/com/example/springbootapirest/service/DepartmentServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.springbootapirest.service;
+
+import com.example.springbootapirest.entity.Department;
+import com.example.springbootapirest.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAddress("Madrid")
+                .departmentCode("IT-01")
+                .departmentId(1L)
+                .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+    }
+
+    @Test
+    @DisplayName("Validar departmentName")
+    public void whenValidDepartmentName_thenDepartmentShouldFound() {
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(departmentName,found.getDepartmentName());
+    }
+
+}
